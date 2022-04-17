4d765d35ffef280c0cf5de538879946a6ead8bd3
sudoshmudo <55032335+sudoshmudo@users.noreply.github.com>
2022-04-17 18:17:54 +0200

    Update CurlFtpFS.sh

--- CurlFtpFS.sh
+++ CurlFtpFS.sh
@@ -12,13 +12,13 @@ sudo doppler setup --no-interactive --config prd
 sudo mkdir /mnt/music
 
 # create mounting service
-sudo doppler run -- envsubst < mnt-music.mount > /etc/systemd/system/mnt-music.mount
+sudo doppler run -- envsubst < ./CurlFtpFS/mnt-music.mount > /etc/systemd/system/mnt-music.mount
 
 # start mounting service
 sudo systemctl enable mnt-music.mount
 
 # create auto-mounting service
-sudo cp mnt-music.automount /etc/systemd/system/mnt-music.automount
+sudo cp ./CurlFtpFS/mnt-music.automount /etc/systemd/system/mnt-music.automount
 
 # start auto-mounting service
 sudo systemctl enable mnt-music.automount
