8e12d44e7c12c0d2dc2b7293cec822ef5809a761
sudoshmudo <55032335+sudoshmudo@users.noreply.github.com>
2022-04-17 18:28:42 +0200

    Create Mopidy.sh

---
+++ Mopidy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# call doppler script here
+
+# call ftp script here
+
+# install Mopidy
+dietpi-software install 118
+
+# install codecs for mopidy
+apt-get install -y gstreamer1.0-plugins-bad
+
+# install mopidy iris
+sudo python3 -m pip install Mopidy-Iris
+
+# give iris needed rights
+sudo sh -c 'echo "mopidy ALL=NOPASSWD: /usr/local/lib/python3.9/dist-packages/mopidy_iris/system.sh" >> /etc/sudoers'
+
+# install mopidy youtube
+sudo python3 -m pip install Mopidy-YouTube
+sudo python3 -m pip install --upgrade ytmusicapi
+sudo python3 -m pip install youtube_dl
+
+# install mopidy spotify
+sudo apt install mopidy-spotify -y
+
+# install mopidy bandcamp
+sudo python3 -m pip install Mopidy-Bandcamp
+
+# install mopidy tunein
+sudo apt install mopidy-tunein -y
