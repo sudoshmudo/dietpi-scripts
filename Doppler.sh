772ea4097049996784d8095f70f575aa86def401
sudoshmudo <55032335+sudoshmudo@users.noreply.github.com>
2022-04-17 17:47:38 +0200

    Create Doppler.sh

---
+++ Doppler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if command -v git &> /dev/null
+then
+    echo "git already installed"
+    exit
+fi
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
+echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
+sudo apt-get update && sudo apt-get install doppler
