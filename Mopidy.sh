d96b03f23443eb12e0f14dd71cabe464ba7f2dd4
sudoshmudo <sudoshmudo>
2022-04-19 13:59:26 +0200

    add username

--- Mopidy.sh
+++ Mopidy.sh
@@ -15,7 +15,7 @@ chmod +x CurlFtpFS.sh
 ./CurlFtpFS.sh
 
 # install Mopidy
-dietpi-software install 118
+/boot/dietpi/dietpi-software install 118
 
 # install codecs for mopidy
 apt-get install -y gstreamer1.0-plugins-bad
@@ -40,14 +40,11 @@ sudo python3 -m pip install Mopidy-Bandcamp
 # install mopidy tunein
 sudo apt install mopidy-tunein -y
 
-# set environment of repo
-sudo doppler setup --no-interactive --config prd
-
 # stop mopidy
-dietpi-services stop mopidy
+/boot/dietpi/dietpi-services stop mopidy
 
 # create mopidy config
 sudo doppler run -- envsubst < ./Mopidy/mopidy.conf > /etc/mopidy/mopidy.conf
 
 # start mopidy
-dietpi-services start mopidy
+/boot/dietpi/dietpi-services start mopidy
