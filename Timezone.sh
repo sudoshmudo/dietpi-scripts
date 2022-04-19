4c59eea84293392a7d053fea085a7b49a4d9f9bc
sudoshmudo <55032335+sudoshmudo@users.noreply.github.com>
2022-04-19 21:47:01 +0200

    Update Timezone.sh

--- Timezone.sh
+++ Timezone.sh
@@ -1,5 +1,5 @@
 
-timezone=(curl https://ipinfo.io/timezone)
+timezone=$(curl https://ipinfo.io/timezone)
 
 ln -s "/usr/share/zoneinfo/$timezone" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
