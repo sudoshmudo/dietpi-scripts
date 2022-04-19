6b4933dfa1148e1d18eef84a69e31f34ff6ed3a4
sudoshmudo <sudoshmudo>
2022-04-19 14:33:07 +0200

    global github

--- Github.sh
+++ Github.sh
@@ -12,6 +12,6 @@ sudo doppler run --command='echo $GITHUB_TOKEN | docker login ghcr.io -u sudoshm
 # add Github credentials
 sudo doppler run -- envsubst < ./Github/.git-credentials > ~/.git-credentials
 
-git config user.email "sudoshmudo"
-git config user.name "sudoshmudo"
+git config --global user.email "sudoshmudo"
+git config --global user.name "sudoshmudo"
 git config credential.helper 'store --file ~/.git-credentials'
