2ce009f347641e6b5249dfb142aff71a9d0433c3
sudoshmudo <sudoshmudo>
2025-07-23 20:04:12 +0200

    update paws script

--- Paws.sh
+++ Paws.sh
@@ -4,6 +4,9 @@ if test -d ~/git/paws; then
     exit
 fi
 
+chmod +x Doppler.sh
+./Doppler.sh
+
 # add Github credentials
 cd ~/git/dietpi-scripts
 apt-get install gettext-base
@@ -24,3 +27,16 @@ cd ~/git/paws
 doppler setup --no-interactive --config prd
 
 pip3 install -r requirements.txt
+
+apt install raspi-config -y
+raspi-config nonint do_i2c 0
+raspi-config nonint do_spi 0
+echo "dtoverlay=spi0-0cs" | tee -a /boot/firmware/config.txt
+
+echo "reboot needed because of hardware config update"
+
+cp ./Paws/paws.service /etc/systemd/system/paws.service
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable paws.service
+systemctl start paws.service
