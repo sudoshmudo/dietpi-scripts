83b93c3f805a68929a58e01bd2fa28974f83daec
sudoshmudo <sudoshmudo>
2026-06-14 18:13:19 +0200

    add personal tracking

---
+++ PersonalTracking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if test -d ~/git/personal-tracking; then
+    exit
+fi
+
+cd cd ~/git/dietpi-scripts
+chmod +x Github.sh
+./Github.sh
+
+chmod +x Docker.sh
+./Docker.sh
+
+cd ~/git
+git clone https://github.com/sudoshmudo/personal-tracking.git
+
+cd personal-tracking
+doppler setup --no-interactive --config prd
+doppler run -- docker compose up -d
